f0853686dd8871e2c1c27fc5537cc30d1bcda160
Matthias Bathke <info@straightvisions.com>
2019-12-02 20:45:07 +0100

    UPDATE

---
+++ usercentrics.php
@@ -0,0 +1,137 @@
+<?php
+namespace sv_tracking_manager;
+
+/**
+ * @version         1.000
+ * @author			straightvisions GmbH
+ * @package			sv_tracking_manager
+ * @copyright		2019 straightvisions GmbH
+ * @link			https://straightvisions.com
+ * @since			1.000
+ * @license			See license.txt or https://straightvisions.com
+ */
+
+class usercentrics extends modules {
+	private $consent_IDs		= array(
+		// bing
+		'sv_tracking_manager_modules_bing_scripts_default'							=> 'Bing Ads',
+
+		// facebook
+		'sv_tracking_manager_modules_facebook_scripts_default'						=> 'Facebook Pixel',
+
+		// google_analytics
+		'sv_tracking_manager_modules_google_analytics_scripts_ga'					=> 'Google Analytics',
+		'sv_tracking_manager_modules_google_analytics_scripts_default'				=> 'Google Analytics',
+		'sv_tracking_manager_modules_google_analytics_scripts_events'				=> 'Google Analytics',
+		'sv_tracking_manager_modules_google_analytics_scripts_events_scroll'		=> 'Google Analytics',
+
+		// google_optimize
+		'sv_tracking_manager_modules_google_optimize_scripts_default'				=> 'Google Optimize',
+		'sv_tracking_manager_modules_google_optimize_scripts_anti_flicker'			=> 'Google Optimize',
+
+		// hotjar
+		'sv_tracking_manager_modules_hotjar_scripts_default'						=> 'Hotjar',
+
+		// linkedin
+		'sv_tracking_manager_modules_linkedin_scripts_default'						=> 'LinkedIn Ads',
+
+		// mouseflow
+		'sv_tracking_manager_modules_mouseflow_scripts_default'					=> 'Mouseflow',
+
+		// yahoo
+		'sv_tracking_manager_modules_yahoo_scripts_default'						=> 'Yahoo Gemini',
+	);
+
+	public function init() {
+		// Section Info
+		$this->set_section_title( __('Usercentrics', 'sv_tracking_manager' ) )
+			->set_section_desc(__( sprintf('%sUsercentrics Login%s', '<a target="_blank" href="https://admin.usercentrics.com/#/">','</a>'), 'sv_tracking_manager' ))
+			->set_section_type( 'settings' )
+			->load_settings()
+			->register_scripts()
+			->get_root()->add_section( $this );
+
+		$this->add_service();
+
+		add_action('init', array($this, 'load'));
+	}
+
+	protected function load_settings(): usercentrics {
+		$this->get_setting('activate')
+			->set_title( __( 'Activate', 'sv_tracking_manager' ) )
+			->set_description('Enable Usercentrics support')
+			->load_type( 'checkbox' );
+
+		$this->get_setting('id')
+			->set_title( __( 'Settings ID', 'sv_tracking_manager' ) )
+			->set_description('The Usercentrics Settings ID for this site.')
+			->load_type( 'text' );
+
+		return $this;
+	}
+	public function is_active(): bool{
+		// activate not set
+		if(!$this->get_setting('activate')->run_type()->get_data()){
+			return false;
+		}
+		// activate not true
+		if($this->get_setting('activate')->run_type()->get_data() !== '1'){
+			return false;
+		}
+		// Setting ID not set
+		if(!$this->get_setting('id')->run_type()->get_data()){
+			return false;
+		}
+		// Setting ID empty
+		if(strlen(trim($this->get_setting('id')->run_type()->get_data())) === 0){
+			return false;
+		}
+		return true;
+	}
+	public function get_consent_IDs(): array{
+		return $this->consent_IDs;
+	}
+	public function get_consent_ID(string $ID): string{
+		return $this->consent_IDs[$ID];
+	}
+	public function has_consent_ID(string $ID): bool{
+		return isset($this->consent_IDs[$ID]) ? true : false;
+	}
+	public function load(): usercentrics{
+		if($this->is_active()){
+			$this->get_script('usercentrics')
+				->set_type('js')
+				->set_is_enqueued()
+				->set_path('https://app.usercentrics.eu/latest/main.js')
+				->set_custom_attributes(' id="'.$this->get_setting('id')->run_type()->get_data().'"');;
+
+			add_filter( 'rocket_minify_excluded_external_js', array($this,'rocket_minify_excluded_external_js') );
+		}
+
+		return $this;
+	}
+	protected function register_scripts(): usercentrics{
+		// Activate Consent Management in Tracking Manager
+		if($this->is_active()) {
+			add_filter('sv_tracking_manager_consent_management', function (bool $active) {
+				return true;
+			});
+
+			add_filter('sv_tracking_manager_data_attributes', function (string $attributes, \sv_core\scripts $script) {
+				if($this->has_consent_ID($script->get_handle())){
+					return $attributes . ' data-usercentrics="'.$this->get_consent_ID($script->get_handle()).'"';
+				}else{
+					return $attributes;
+				}
+			}, 10, 2);
+		}
+
+		return $this;
+	}
+	// never combine external JS
+	public function rocket_minify_excluded_external_js($pattern){
+		$pattern[] = 'usercentrics';
+
+		return $pattern;
+	}
+}
